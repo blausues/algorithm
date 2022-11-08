6c990269f80b27e362fd92282571f34b8a40131b
blausues <blausues@gmail.com>
2022-11-08 23:19:46 +0900

    [level 2] Title: 올바른 괄호, Time: 3.21 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,9 @@
+function solution(s){
+    let cnt = 0;
+    for (let i=0; i<s.length; i++) {
+        if (s[i] === '(') cnt++;
+        else if (s[i] === ')') cnt--;
+        if (cnt < 0) return false;
+    }
+    return cnt === 0;
+}
