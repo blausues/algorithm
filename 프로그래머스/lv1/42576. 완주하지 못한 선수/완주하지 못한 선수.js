896e712c088fe886e141a9212adf86b8e6d7ab1c
blausues <blausues@gmail.com>
2022-11-22 23:18:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 27.84 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,13 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for (let i=0; i<participant.length; i++) {
-        if (participant[i] !== completion[i]) return participant[i];
+    const nameMap = new Map();
+    participant.forEach((p, i) => {
+        const c = completion[i];
+        nameMap.set(p, (nameMap.get(p) || 0) + 1);
+        if (c) {
+            nameMap.set(c, (nameMap.get(c) || 0) - 1);    
+        }
+    });
+    for (const n of nameMap) {
+        if (n[1] > 0) return n[0];
     }
 }
