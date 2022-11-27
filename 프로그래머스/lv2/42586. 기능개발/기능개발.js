62a74f6f5a9eb09ef617f024fd2203e217a31b55
blausues <blausues@gmail.com>
2022-11-28 01:08:21 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,20 +1,14 @@
 function solution(progresses, speeds) {
-    let result = [];
-    let endDay = [];
-    
-    progresses.forEach((v, i) => {
-        endDay.push(Math.ceil((100 - v) / speeds[i]));
-    })
+    const endDay = progresses.map((v, i) => Math.ceil((100 - v) / speeds[i]));
     
     let lastEndDay = 0;
-    endDay.forEach(v => {
+    return endDay.reduce((acc, v) => {
         if (v <= lastEndDay) {
-            result[result.length - 1] += 1;
+            acc[acc.length - 1] += 1;
         } else if (v > lastEndDay) {
-            result.push(1);
+            acc.push(1);
             lastEndDay = v;
         }
-    })
-    
-    return result;
+        return acc;
+    }, []);
 }
